5e74fcf8b5e51967f441ec8e13083ff5a26c513e
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-06-17 15:30:29 +0200

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="bg-red-500 font-black">
+      Hi vite
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
