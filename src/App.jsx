1c63c29a366b02bd716e1e8fc384bb355d4ecb2f
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-06-18 14:12:17 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,40 @@ import Header from "./components/Header";
 import IconController from "./components/IconController";
 import SideNav from "./components/SideNav";
 import LogoPreview from "./components/LogoPreview";
-import { UpdateStorageContext } from "./context/UpdateStorageContext";
+import { UpdateStorageProvider,useUpdateStorage } from "./context/UpdateStorageContext";
+
 
 function App() {
-  const [selectedIndex, setSelectedIndex] = useState(1);
-  const [updateStorage, setUpdateStorage] = useState({});
-
   return (
-    <UpdateStorageContext.Provider value={{ updateStorage, setUpdateStorage }}>
+    <UpdateStorageProvider>
+      <Content/>
+    </UpdateStorageProvider>
+  );
+}
+
+function Content () {
+  const { storage, updateStorage } = useUpdateStorage();
+  const {pageIndex} = storage
+
+  return(
+    <>
       <Header />
       <div className="fixed top-20 w-64">
-        <SideNav
-          selectedIndex={selectedIndex}
-          setSelectedIndex={setSelectedIndex}
-        />
+        <SideNav/>
       </div>
       <div className="ml-64 mt-20 grid grid-cols-1 overflow-auto xl:grid-cols-6">
-        <div className="h-screen border p-5 shadow-sm xl:col-span-2">
-          {selectedIndex === 1 ? <IconController /> : <BackgroundController />}
+        <div className="h-fit xl:h-screen border p-5 shadow-sm xl:col-span-2">
+          {pageIndex === 1 ? <IconController /> :  <BackgroundController />}
         </div>
         <div className="xl:col-span-3">
           <LogoPreview />
         </div>
-        <div className="xl:col-span-1">Adds</div>
+        <div className="xl:col-span-1">
+        Add
+        </div>
       </div>
-    </UpdateStorageContext.Provider>
-  );
+    </>
+  )
 }
 
 export default App;
