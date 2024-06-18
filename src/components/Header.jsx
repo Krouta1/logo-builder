79ee2e04706ecedec009837b347fd743c0848918
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-06-18 14:09:03 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <div className="fixed top-0 flex h-20 w-full items-center justify-between border p-4 shadow-sm backdrop-blur">
+    <div className="fixed top-0 flex h-20 w-full items-center justify-between border p-4 shadow-sm backdrop-blur z-50">
       <img src="./logo.svg" alt="App logo" />
       <Button className="flex items-center gap-2">
         <DownloadIcon className="h-4 w-4" />
