0bbd3f48b3fefb187b329f0b89347811616b51c6
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-06-18 14:08:05 +0200

    Update SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { cn } from "../lib/utils";
 import { ImageIcon, PencilRulerIcon, ShieldIcon } from "lucide-react";
-import { useState } from "react";
+import {useUpdateStorage} from "../context/UpdateStorageContext"
 
 const menuList = [
   {
@@ -21,19 +21,26 @@ const menuList = [
   },
 ];
 
-const SideNav = ({ selectedIndex, setSelectedIndex }) => {
+const SideNav = () => {
+  const { storage, updateStorage } = useUpdateStorage();
+  const {pageIndex} = storage
+
+  const handleIndexChange = (newIndex) => {
+    updateStorage({ pageIndex: newIndex });
+  };
+
   return (
     <div className="h-screen border">
       {menuList.map((menu) => (
         <h2
           key={menu.id}
           onClick={() => {
-            setSelectedIndex(menu.id);
-            setSelectedIndex(menu.id);
+            handleIndexChange(menu.id);
+            handleIndexChange(menu.id);
           }}
           className={cn(
             "hover:bg-primary my-2 flex cursor-pointer items-center gap-2 rounded-md px-7 py-3 text-lg font-bold text-gray-500 transition-all hover:text-white",
-            selectedIndex === menu.id && "bg-primary text-white",
+            pageIndex === menu.id && "bg-primary text-white",
           )}
         >
           <menu.icon className="h-6 w-6" />
