2df1f31048b83b8701a51ebad77d27a847ac925d
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-06-18 14:08:27 +0200

    Update LogoPreview.jsx

--- src/components/LogoPreview.jsx
+++ src/components/LogoPreview.jsx
@@ -1,16 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { icons } from "lucide-react";
-import { UpdateStorageContext } from "../context/UpdateStorageContext";
-import { useContext, useEffect } from "react";
+import {useUpdateStorage} from "../context/UpdateStorageContext"
 
 const LogoPreview = () => {
-  const { updateStorage, setUpdateStorage } = useContext(UpdateStorageContext);
 
-  useEffect(() => {
-    const storageData = JSON.parse(localStorage.getItem("value"));
-    setUpdateStorage(storageData);
-  }, [updateStorage, setUpdateStorage]);
+  const { storage, updateStorage } = useUpdateStorage();
+  const {bgRounded, bgPadding,bgColor,iconSize, iconRotate,iconColor,icon} = storage
+
 
   const Icon = ({ name, size, color }) => {
     const LucidIcon = icons[name];
@@ -20,7 +17,7 @@ const LogoPreview = () => {
       <LucidIcon
         size={size}
         color={color}
-        style={{ transform: `rotate(${updateStorage?.iconRotate}deg)` }}
+        style={{ transform: `rotate(${iconRotate}deg)` }}
       />
     );
   };
@@ -30,20 +27,20 @@ const LogoPreview = () => {
       <div
         className="h-[400px] w-[400px] bg-gray-200 outline-dotted outline-gray-300"
         style={{
-          padding: `${updateStorage?.bgPadding}px`,
+          padding: `${bgPadding}px`,
         }}
       >
         <div
           className="flex h-full w-full items-center justify-center"
           style={{
-            borderRadius: `${updateStorage?.bgRounded}%`,
-            background: updateStorage?.bgColor,
+            borderRadius: `${bgRounded}%`,
+            background: bgColor,
           }}
         >
           <Icon
-            size={updateStorage?.iconSize}
-            color={updateStorage?.iconColor}
-            name={updateStorage?.icon}
+            size={iconSize}
+            color={iconColor}
+            name={icon}
           />
         </div>
       </div>
@@ -51,37 +48,5 @@ const LogoPreview = () => {
   );
 };
 
-// export default LogoPreview;
+export default LogoPreview;
 
-// import React from "react";
-// import { useUpdateStorage } from "./UpdateStorageContext";
-
-// const BackgroundController = () => {
-//   const { storage, updateStorage } = useUpdateStorage();
-
-//   // Handlers to update context values
-//   const handleRoundedChange = (newRounded) => {
-//     updateStorage({ bgRounded: newRounded });
-//   };
-
-//   const handlePaddingChange = (newPadding) => {
-//     updateStorage({ bgPadding: newPadding });
-//   };
-
-//   const handleColorChange = (newColor) => {
-//     updateStorage({ bgColor: newColor });
-//   };
-
-//   // Use storage values directly from context
-//   return (
-//     <div>
-//       {/* Example usage */}
-//       <input
-//         type="number"
-//         value={storage.bgRounded}
-//         onChange={(e) => handleRoundedChange(e.target.value)}
-//       />
-//       {/* Similar for padding and color */}
-//     </div>
-//   );
-// };
